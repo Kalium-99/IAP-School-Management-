561e804fa7138dc748d2ff10f413637ec8c9cbec
Celestine Hope <celestinehope17@gmail.com>
2022-12-22 00:50:12 +0300

    Merge branch 'jay'

---
+++ resources/views/list.blade.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="{{asset('stylecss/liststyle.css')}}">
+
+<div class ="listbox">
+<h1> Course list </h1>
+<table border="1">
+    
+    
+    <tr>
+ <td> Course ID </td>
+ <td>  Course name </td>
+ <td> Unit no. </td>
+ <td> Description </td>
+ </tr>
+ @foreach ($courses as $course)
+
+<tr>
+ <td>{{$course['course_no']}}</td>
+ <td>{{$course['course_name']}}</td>
+ <td> {{$course['unit_no']}} </td>
+ <td> {{$course['description']}} </td>
+ 
+</tr>
+@endforeach
+
+
+</table>
+</div>
+
