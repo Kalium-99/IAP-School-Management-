e3e82b0abae3d84d98fcdc9895441e808e14e039
pjaaaay <boruettpj@gmail.com>
2023-01-02 03:13:21 +0300

    changes

--- resources/views/worksubmission.blade.php
+++ resources/views/worksubmission.blade.php
@@ -13,10 +13,16 @@
 <div class="listbox">
 <form action="upload" method="post">
     @csrf
-<h1>baby baby</h1>
-    <h4> my love my love</h4>      
-         <h5> hiiii</h5>
+    @foreach ($courseworks as $coursework)
 
+
+    <<h2>{{$coursework['title']}}</h2>
+        <h3> {{$coursework['description']}}</h3>      
+             <h5>{{$coursework['deadline']}}</h5>
+    
+    
+    
+    @endforeach
 
          <input type="file" name="submission"><br><br>
          <button type="submit">Upload file</button>
@@ -30,13 +36,4 @@
 
 
 
-@foreach ($courseworks as $coursework)
 
-
-<<h2>{{$coursework['title']}}</h2>
-    <h3> {{$coursework['description']}}</h3>      
-         <h5>{{$coursework['deadline']}}</h5>
-
-
-
-@endforeach
